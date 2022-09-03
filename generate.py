c45f20cc485a2c4ac63914ed18277701934440a9
Maroderone <67072123+Maroderone@users.noreply.github.com>
2022-09-03 21:19:34 +0300

    Update generate.py

--- generate.py
+++ generate.py
@@ -1,1 +1,19 @@
+import pickle
+import re
+import random
+import sys
 
+def generate(model, length, seed=42, prefix = None):
+        with open(f'{model}', 'rb') as f:
+            grams = pickle.load(f)
+        if prefix is None:
+            prefix = random.choice(list(grams.values())[:-1])[0]
+        text=''
+        random.seed = seed
+        print(grams)
+        for i in range(length):
+            next_word = random.choice(grams[prefix])
+            text += ' '
+            text += next_word
+            prefix = next_word
+        print(text)
